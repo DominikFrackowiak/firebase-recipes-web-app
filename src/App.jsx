2a765a091ed4bc420895fb1e325d4a96caea9e1c
DominikFrackowiak <rorschach@op.pl>
2023-03-22 16:57:26 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+// import firebase from './FirebaseConfig'
+
+function App() {
+
+
+	return (
+		<div className='App'>
+			<div className='title-row'>
+				<h1 className='title'>Firebase Recipes</h1>
+			</div>
+		</div>
+	)
+}
+
+export default App
